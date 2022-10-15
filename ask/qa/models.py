a2f0b73bf9d2bde9e0069b8e063761dc304e20f9
Mark Sol <114338588+Mark-Sol111@users.noreply.github.com>
2022-10-16 02:55:07 +1100

    add default values for models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,13 +6,13 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateField()
+    title = models.CharField(max_length=255,default='')
+    text = models.TextField(default='')
+    added_at = models.DateField(null=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, related_name='q_author_user',
                                on_delete=models.CASCADE, null=True)
-    likes = models.ManyToManyField(User, related_name='q_likes_users')
+    likes = models.ManyToManyField(User, related_name='q_likes_users',null=True)
     # objects = QuestionManager()
 
     def __str__(self):
@@ -38,10 +38,10 @@ class QuestionManager(models.Manager):
 
 
 class Answer(models.Model):
-    text = models.TextField()
-    added_at = models.DateField()
-    questtion = models.ForeignKey(Question)
-    author = models.ForeignKey(User)
+    text = models.TextField(default='')
+    added_at = models.DateField(null=True)
+    questtion = models.ForeignKey(Question,null=True)
+    author = models.ForeignKey(User,null=True)
 
     def __str__(self):
         return self.text
